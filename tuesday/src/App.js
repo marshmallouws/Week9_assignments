3f8ac181cf11e6d48bceead4241b3060808dc5b3
Marshmallouws <e_hlers@live.dk>
2019-10-29 14:15:26 +0100

    somethign

--- tuesday/src/App.js
+++ tuesday/src/App.js
@@ -1,31 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function ShowCount(props) {
-  return props.count;
-}
-
-function App() {
-
+function SetStart(props) {
+  
   //Syntax for getter and setter methods
-  const [count, setCount] = useState(0);
-  const counter = <ShowCount count={10}/>;
+  const [count, setCount] = useState(props.start);
+  /*const counter = <ShowCount count={10}/>;
 
   useEffect(() => {
     document.getElementById("counter").innerHTML = `You clicked ${counter} times`;
-  });
+  }); */
 
   return (
     <div className="App">
-      <h1 id='counter'></h1>
-      <button onClick={() => setCount(count + 1)}>
+      <h1>You have pressed {count} times</h1>
+      <button onClick={() => setCount(count + props.increment)}>
         Click me!
       </button>
-      <button onClick={() => setCount(count-1)}>
+      <button onClick={() => setCount(count - props.decrement)}>
         Click me :)
       </button>
     </div>
   );
+}
+
+function App() {
+  <SetStart start={10} increment={1} decrement={1}/>
 }
 
 export default App;
